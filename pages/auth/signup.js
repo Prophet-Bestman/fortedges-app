31a1341d5c8d53416a80138d34c72f7d183275b8
Prophet-Bestman <bestiproph@gmail.com>
2022-03-17 07:00:31 +0100

    installed react-hook-form and fontsource DM sans

---
+++ pages/auth/signup.js
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <Box minH="100vh" bg="app.primary">
+      Signup
+    </Box>
+  );
+};
+
+export default Signup;
