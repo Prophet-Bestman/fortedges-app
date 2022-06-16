f3d37b46b76a9b3572cc55b365572081bbde6ea7
Prophet-Bestman <bestiproph@gmail.com>
2022-06-16 10:50:51 +0100

    Linked to demo

--- api/baseUrls.js
+++ api/baseUrls.js
@@ -1,2 +1,5 @@
-export const landingUrl = "https://ubassets.com";
+const demo = "https://fortedges-web-demo.netlify.app/";
+const production = "https://fortedges-web-demo.netlify.app/";
+
+export const landingUrl = demo;
 export const baseUrl = "https://ubassets-api.herokuapp.com";
